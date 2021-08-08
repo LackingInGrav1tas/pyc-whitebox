73f4f7d46bf2d4f71b4e457fe255f3028fa4cff2
LackingInGrav1tas <42680395+LackingInGrav1tas@users.noreply.github.com>
2021-08-08 01:00:23 -0400

    trying my best

--- c_templates/main.cpp
+++ c_templates/main.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <vector>
 
 #include "c_templates/AES.h"
 
@@ -12,15 +13,20 @@ void write_to_file(unsigned char *bytes, const char *fname) {
 }
 
 int main(int argc, char** argv) {
-    int pc = 0;
     unsigned char plain[] = /*plaintext*/;
-    unsigned char key[] = /*key*/;
+    unsigned char key[] = /*key*/;    
 
-    // deobfuscate
+    // use self editing VM working backwards in the python program to use here
+    {
+        // shiftable opcode mappings
+        char mappings[] = { 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xAA };
+        std::vector<char> opcode = /*opcode*/;
+        
+    }
 
     unsigned int size = 16 * sizeof(unsigned char);
     AES aes(128);
     unsigned char *decrypted = aes.DecryptECB(plain, size, key);
-    
+
     write_to_file(decrypted, "dec.bin");
 }
