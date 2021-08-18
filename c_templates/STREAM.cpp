0ebff5722ff88958ae7b8817399f08cb32acc271
LackingInGrav1tas <42680395+LackingInGrav1tas@users.noreply.github.com>
2021-08-17 20:44:05 -0400

    but actually this time

--- c_templates/STREAM.cpp
+++ c_templates/STREAM.cpp
@@ -1,7 +1,10 @@
+#include <iostream>
+
 unsigned char* stream_cipher(unsigned char* key, unsigned char* plaintext, int size) {
     unsigned char* result = new unsigned char[size];
     for (int i = 0; i < size; i++) {
-        result[i] = key[i] & plaintext[i];
+        result[i] = key[i] ^ plaintext[i];
+        // std::cout << (char)result[i] << " = " << (char)key[i] << " ^ " << (char)plaintext[i] << std::endl;
     }
     return result;
 }
